23752e0d8580efea05187c5953c6bbada0784ebe
CalderMacLean <caldmac08@gmail.com>
2017-05-27 02:01:53 +0100

    Add files via upload
    Template on which to build for proper function of reading gps data from arduino to chip

---
+++ CanSat/gpsdata.py
@@ -0,0 +1,13 @@
+def read_ard():
+	global gps_data, ard_err, ard_data
+	gps_data = []
+	ard_data[i] = bus.read_byte(ard_addr)
+	except IOError, err:
+                ard_err = True
+	gps_data[0]  = chr(ard_data[6])+chr(ard_data[7])+chr(ard_data[8])+chr(ard_data[9])+chr(ard_data[10])+chr(ard_data[11])+chr(ard_data[12])+chr(ard_data[13]) #longitude
+        gps_data[1]  = int(str(ard_data[14])) #longitude direction
+        gps_data[2]  = chr(ard_data[15])+chr(ard_data[16])+chr(ard_data[17])+chr(ard_data[18])+chr(ard_data[19])+chr(ard_data[20])+chr(ard_data[21])+chr(ard_data[22]) #latitude
+        gps_data[3]  = int(str(ard_data[23])) #latitude direction
+        gps_data[4]  = chr(ard_data[24])+chr(ard_data[25]) #speed
+        gps_data[5]  = int(str(ard_data[26])) #validity
+
