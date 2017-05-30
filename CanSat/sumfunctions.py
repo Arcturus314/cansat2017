40d98d7598eda4d9d42418594576a4bfcc8b8ba6
CalderMacLean <caldmac08@gmail.com>
2017-05-30 17:25:21 +0100

    Update sumfunctions.py

--- CanSat/sumfunctions.py
+++ CanSat/sumfunctions.py
@@ -6,8 +6,8 @@ global sumt1, sumt2, sumt3, sumt4, sumt5
 	sumt3 = 0
 	sumt4 = 0
 	sumt5 = 0
-	sumt1 = get_accelerometer_data(False) / 5
-	sumt2 = get_magnetometer_data(False) / 5
-	sumt3 = get_gyroscope_data(False) / 6
-	sumt4 = ( get_env_pressure_data(False) + get_env_humidity_data(False) + get_env_temp_data(False)) / 7
-	sumt5 = get_temp_array_data(False) / 17
+	sumt1 = datastore.get_accelerometer_data(False) / 5
+	sumt2 = datastore.get_magnetometer_data(False) / 5
+	sumt3 = datastore.get_gyroscope_data(False) / 6
+	sumt4 = ( datastore.get_env_pressure_data(False) + datastore.get_env_humidity_data(False) + datastore.get_env_temp_data(False)) / 7
+	sumt5 = datastore.get_temp_array_data(False) / 17
