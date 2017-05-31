f53b2aa179cf59f884b9945140549038ba2e21c5
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-05-31 14:35:45 -0700

    finished v1 of packetization script

--- CanSat/packet.py
+++ CanSat/packet.py
@@ -1,4 +1,6 @@
 import datastore
+import position
+import tempmap
 
 #this module will serve to facilitate the building of packets for transmission to the base station
 
@@ -22,11 +24,11 @@ import datastore
 #packet settings will be stored in the following set of variables
 
 #indicates whether individual sensor data points should be included
-#[accelerometer, magnetometer, gyroscope, imu_temp, env_pressure, env_humidity]
-inc_data = [True, True, True, True, True, True]
+#[accelerometer, magnetometer, gyroscope, imu_temp, env_pressure, env_humidity, env_temp]
+inc_data = [True, True, True, True, True, True, True]
 #indicates whether all sensor data points should be included
-#[accelerometer, magnetometer, gyroscope, imu_temp, env_pressure, env_humidity]
-inc_all_data = [False, False, False, False, False, False]
+#[accelerometer, magnetometer, gyroscope, imu_temp, env_pressure, env_humidity, env_temp]
+inc_all_data = [False, False, False, False, False, False, False]
 #indicates whether error values should be included
 inc_error = True
 #indicates whether current cansat position should be included
@@ -38,78 +40,98 @@ inc_map = True
 
 #Each potential data value will have an associated identifier
 #stored in a dictionary
-#'I' denotes individual point; 'A' denotes all points
-id_dict = ['AccelI': 0x00, 'MagI': 0x01, 'GyroI': 0x02, 'iTempI': 0x03, 'ePresI': 0x04, 'eHumI': 0x05, 'eTempI': 0x06]
+id1_dict = {'Default': 0x00, 'Accel': 0x01, 'Mag': 0x02, 'Gyro': 0x03, 'iTemp': 0x04, 'ePres': 0x05, 'eHum': 0x06, 'eTemp': 0x07, 'Error': 0x08, 'Pos': 0x09, 'Mat': 0x0A, 'Map': 0x0B}
+#Secondary identifier allows for differentiation between values of the same sensor
+#Ex single value has id2 = 0x00 double value has id2 = 0x01
+id2_dict = {'Default': 0x00, 'Single': 0x01, 'All': 0x02}
 
-packet_type= 0
+#Name list to simplify code: tracks index vs sensor type for inc_data and inc_all_data
+index_names = ['Accel', 'Mag', 'Gyro', 'iTemp', 'ePres', 'eHum', 'eTemp']
+method_names = ['get_accelerometer_data','get_magnetometer_data','get_gyroscope_data','get_imu_temp_data','get_env_pressure_data','get_env_humidity_data','get_env_temp_data']
+
+packet_type = 0
+checksum_contribution = 0
 
 #Packets will be structured as follows
 # --header--
 # ':,(num messages in body),|'
 # ---body---
-# '(identifier),(data1),(data2),...,;'
-# '(identifier),(data1),(data2),...,;' 
+# '(identifier1),(identifier2),(data1),(data2),...,;'
+# '(identifier1),(identifier2),(data1),(data2),...,;' 
 # ...
 # '|'
 # --footer--
 # (checksum)
 
+def create_message(identifier1, identifier2, data):
+    global checksum_contribution
+    message = id1_dict[identifier1] + ',' + id2_dict[identifier2] + ','
+    checksum_contribution = checksum_contribution + int(id1_dict[identifier1]) + int(id2_dict[identifier2])
+    if type(data) = tuple:
+        for i in xrange(data.len()):
+            message = message + data[i] + ','
+            checksum_contribution = checksum_contribution + int(data[i])
+    message = message + ';'
+    return message
+
 def init_packet(_inc_data,_inc_all_data,_inc_error,_inc_pos,_inc_mat,_inc_map):
-    global inc_data,inc_all_data,inc_error,inc_pos,inc_mat,inc_map
+    global inc_data,inc_all_data,inc_error,inc_pos,inc_mat,inc_map,checksum_contribution
+    checksum_contribution = 0
     inc_data = _inc_data
     inc_all_data = _inc_all_data
     inc_error = _inc_error
     inc_pos = _inc_pos
     inc_mat = _inc_mat
     inc_map = _inc_map
-def init_packet(_packet_type):
-    global inc_data,inc_all_data,inc_error,inc_pos,inc_mat,inc_map,packet_type
+def init_packet_type(_packet_type):
+    global inc_data,inc_all_data,inc_error,inc_pos,inc_mat,inc_map,packet_type, checksum_contribution
+    checksum_contribution = 0
     packet_type = _packet_type
     if packet_type == 1:
-        inc_data  = [True, True, True, True, True, True]
-        inc_all_data = [False, False, False, False, False, False]
+        inc_data  = [True, True, True, True, True, True, True]
+        inc_all_data = [False, False, False, False, False, False, True]
         inc_error = False
         inc_pos = False
         inc_map = False
         inc_mat = False
     if packet_type == 2:
-        inc_all_data = [False, False, False, True, True, True]
-        inc_data = [False, False, False, False, False, False]
+        inc_all_data = [False, False, False, True, True, True, True]
+        inc_data = [False, False, False, False, False, False, False]
         inc_error = False
         inc_pos = False
         inc_mat = False
         inc_map = False
     if packet_type == 3:
-        inc_data = [True, True, True, False, True, False]
-        inc_all_data = [False, False, False, False, False, False]
+        inc_data = [True, True, True, False, True, False, False]
+        inc_all_data = [False, False, False, False, False, False, False]
         inc_error = True
         inc_pos = True
         inc_mat = False
         inc_map = False
     if packet_type == 4:
-        inc_data = [False, False, False, False, False, False]
-        inc_all_data = [False, False, False, False, False, False]
+        inc_data = [False, False, False, False, False, False, False]
+        inc_all_data = [False, False, False, False, False, False, False]
         inc_error = False
         inc_pos = True
         inc_mat = True
         inc_map = False
     if packet_type == 5:
-        inc_data = [False, False, False, False, False, False]
-        inc_all_data = [False, False, False, False, False, False]
+        inc_data = [False, False, False, False, False, False, False]
+        inc_all_data = [False, False, False, False, False, False, False]
         inc_error = False
         inc_pos = False
         inc_mat = False
         inc_map = True
     if packet_type == 6:
-        inc_data = [False, False, False, False, False, False]
-        inc_all_data = [True, True, True, True, True, True]
+        inc_data = [False, False, False, False, False, False, False]
+        inc_all_data = [True, True, True, True, True, True, False]
         inc_error = False
         inc_pos = False
         inc_mat = False
         inc_map = False
     if packet_type = 7:
-        inc_data = [False, False, False, False, False, False]
-        inc_all_data = [False, False, False, False, False, False]
+        inc_data = [False, False, False, False, False, False, False]
+        inc_all_data = [False, False, False, False, False, False, False]
         inc_error = True
         inc_pos = False
         inc_mat = False
@@ -134,13 +156,37 @@ def build_header():
     if inc_map == True:
         messages = messages + 1        
     
-    header = header + str(messages)
-
+    header = header + str(messages) + '|\n'
     return header
 def build_body():        
     body = "" #string to hold body message
-    
-    #Now need to add all relevant data in header and message system
-    if packet_type != 0:
-        if packet_type = 1:
-            data =  datastore.read_all_active()
+    for i in xrange(inc_data.len()):
+        if inc_data[i] == True:
+            body = body + create_message(id1_dict[index_names[i]],'Single',getattr(datastore.method_names[i])(False))
+        body = body + ';\n'
+    for i in xrange(inc_all_data.len()):
+        if inc_all_data[i] == True:
+            body = body + create_message(id1_dict[index_names[i]],'All',getattr(datastore.method_names[i])(True))
+        body = body + ';\n'
+
+    if inc_error == True:
+        body = body + create_message(id1_dict['Error'],id2_dict['Default'],datastore.get_errors())
+        body = body + ';\n'
+    if inc_pos == True:
+        body = body + create_message(id1_dict['Pos'],id2_dict['Default'],position.get_pos())
+        body = body + ';\n'
+    if inc_mat == True:
+        body = body + create_message(id_dict['Mat'],id2_dict['Default'],datastore.get_temp_array_data())
+        body = body + ';\n'
+    if inc_map == True:
+        body = body + create_message(id_dict['Map'],id2_dict['Default'],tempmat.get_temp_map())
+        body = body + ';\n'
+
+    body = body + '|\n'
+    return body
+def build_footer():
+    global checksum_contribution
+    return checksum_contribution % 251 #as 251 is the largest prime number less than 255
+
+def build_packet():
+    return build_header() + build_body() + build_footer() 
