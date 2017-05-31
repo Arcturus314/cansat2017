b72b8aa23748dfcf17393915a6c5c5e2ee35ee8f
CalderMacLean <caldmac08@gmail.com>
2017-05-31 22:52:06 +0100

    Update combined_arduino_script.cpp

--- CanSat/combined_arduino_script.cpp
+++ CanSat/combined_arduino_script.cpp
@@ -510,3 +510,50 @@ boolean Adafruit_GPS::wakeup(void) {
       return false;  // Returns false if not in standby mode, nothing to wakeup
   }
 }
+
+void setup(){
+   Wire.begin(x);
+   Wire.onRecieve(recieveEvent);
+   serial.begin(9600);
+	
+}
+	Sim800l.begin(); // initializate the library. 
+  if ( xbee_err = 1) {
+	text="1";  //text for the message. 
+	number="07415606808"; //change to a valid number.
+	error=Sim800l.sendSms(number,text);
+	// OR 
+	//Sim800l.sendSms("+540111111111","the text go here")
+}
+  if ( lsm303_err = 2 ) {
+  text="2";  //text for the message. 
+  number="07415606808"; //change to a valid number.
+  error=Sim800l.sendSms(number,text);
+  // OR 
+  //Sim800l.sendSms("+540111111111","the text go here")
+}
+if ( l3gd20_err = 4 ) {
+  text="3";  //text for the message. 
+  number="07415606808"; //change to a valid number.
+  error=Sim800l.sendSms(number,text);
+  // OR 
+  //Sim800l.sendSms("+540111111111","the text go here")
+}
+if (bme280_err = 8 ) {
+  text="4";  //text for the message. 
+  number="07415606808"; //change to a valid number.
+  error=Sim800l.sendSms(number,text);
+  // OR 
+  //Sim800l.sendSms("+540111111111","the text go here")
+}
+if ( d6t_err = 16 ) {
+  text="5";  //text for the message. 
+  number="07415606808"; //change to a valid number.
+  error=Sim800l.sendSms(number,text);
+  // OR 
+  //Sim800l.sendSms("+540111111111","the text go here")
+} 
+
+void loop(){
+  //do nothing
+}
