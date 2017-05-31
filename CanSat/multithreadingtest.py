206a609c89d109618afa884ca3f51dd804e66753
CalderMacLean <caldmac08@gmail.com>
2017-05-31 17:48:46 +0100

    Update multithreadingtest.py

--- CanSat/multithreadingtest.py
+++ CanSat/multithreadingtest.py
@@ -1,4 +1,5 @@
 import threading
+import datastore
 
 class myThread(threading.Thread):
 	def _init_(slef, threadID, name):
@@ -6,7 +7,79 @@ class myThread(threading.Thread):
 		self.name = name
 	def run(self):
 		
-def data():
+def send_data():
+	print ":",
+	print 0x00,
+	print ",",
+	print 0x00,
+	print datastore.get_accelerometer_data(False),
+	print 0xff,
+	print ",",
+	print 0xff,
+	print ":",
+	print 0x01,
+	print ",",
+	print 0x01,
+	print ",",
+	print datastore.magnetometer(False),
+	print 0xff,
+	print ",",
+	print 0xff,
+	print ":",
+	print 0x02,
+	print ",",
+	print 0x02,
+	print ",",
+	print datastore.get_gyroscope_data(False),
+	print 0xff,
+	print ",",
+	print 0xff,
+	print ":",
+	print 0x03,
+	print ",",
+	print 0x03,
+	print ",",
+	print datastore.get_env_pressure__data(False),
+	print 0xff,
+	print ",",
+	print 0xff,
+	print ":"
+	print 0x04,
+	print ",",
+	print 0x04,
+	print datastore.get_env_humidity_data(False),
+        print 0xff,
+        print ",",
+        print 0xff,
+        print":"
+        print 0x05,
+        print ",",
+        print 0x05,
+        print ",",
+        print datastore.get_env_temp_data(False),
+        print ",",
+        print 0xff,
+        print ",",
+        print 0xff,
+        print":"
+        print 0x06,
+        print ",",
+        print 0x06,
+        print ",",
+        print datastore.get_temp_array_data(False),
+        print ",",
+        print 0xff,
+        print ",",
+        print 0xff,
+	#print ":"
+	##print 0x07,
+	###print ",",
+	####print 0x07,
+	#print gps_data,
+	#####print ",",
+	#print 0xff,
+	#print ",",
+#print 0xff
 
 thread1 = myThread(1, "Thread-1", 1)
 
