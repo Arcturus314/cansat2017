2f37ead2bad370c033f3b646d3e6e2d3b0fa073a
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-06-02 04:27:43 -0700

    added method to find sensor scales

--- CanSat/sensor_tests.py
+++ CanSat/sensor_tests.py
@@ -1,9 +1,21 @@
 import datastore
 
+scale_sum
+num
+
 def find_accel_scale(power, update, deflection):
+    global scale_sum,num
     datastore.set_accelerometer_settings(power,update,deflection)
     adata = datastore.get_accelerometer_data(False)
     vsum = (adata[0]**2.0+adata[1]**2.0+adata[2]**2.0)**0.5
     scale = 9.81/vsum
+    scale_sum = scale_sum + scale
+    num = num + 1
     return vsum,scale
+
+def find_mean_scale()
+    global scale_sum,num
+    return scale_sum/float(num)
+
+
 
