afa5471e63ef696a42e2a243d67b5a7489401290
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-06-02 04:42:30 -0700

    added magnetometer test methods

--- CanSat/sensor_tests.py
+++ CanSat/sensor_tests.py
@@ -12,10 +12,15 @@ def find_accel_scale(power, update, deflection):
     scale_sum = scale_sum + scale
     num = num + 1
     return vsum,scale
-
 def find_mean_scale():
     global scale_sum,num
     return scale_sum/float(num)
 
-
+def find_mag_scale(power, update, deflection):
+    global scale_sum,num
+    datastore.set_magnetometer_settings(power,update,deflection)
+    adata = datastore.get_magnetometer_data(False)
+    vsum = (adata[0]**2.0+adata[1]**2.0+adata[2]**2.0)**0.5
+    return adata[0],adata[1],adata[2],vsum
+   
 
