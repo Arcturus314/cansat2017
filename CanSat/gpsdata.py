766f23ff65dd683dd6417fbab3c319f98e2323b8
CalderMacLean <caldmac08@gmail.com>
2017-06-04 20:10:52 +0100

    Update gpsdata.py

--- CanSat/gpsdata.py
+++ CanSat/gpsdata.py
@@ -8,10 +8,25 @@ def read_ard():
 		ard_data[i] = smbus.read_byte_data(ard_addr)
 	except IOError, err:
                 ard_err = True
-	gps_data[0]  = chr(ard_data[6])+chr(ard_data[7])+chr(ard_data[8])+chr(ard_data[9])+chr(ard_data[10])+chr(ard_data[11])+chr(ard_data[12])+chr(ard_data[13]) #longitude
-        gps_data[1]  = int(str(ard_data[14])) #longitude direction
-        gps_data[2]  = chr(ard_data[15])+chr(ard_data[16])+chr(ard_data[17])+chr(ard_data[18])+chr(ard_data[19])+chr(ard_data[20])+chr(ard_data[21])+chr(ard_data[22]) #latitude
-        gps_data[3]  = int(str(ard_data[23])) #latitude direction
-        gps_data[4]  = chr(ard_data[24])+chr(ard_data[25]) #speed
-        gps_data[5]  = int(str(ard_data[26])) #validity
+	if (ard_data[0] == 0x00 && ard_data[1] == 0x00 && ard_data[3] = 0xFF && ard_data[4] = 0xFF):
+		try:
+			gpsdata[0] = ard_data[2]
+		
+        except ValueError, err:
+		ard_err = True
+	return none 
+	if (ard_data[5] = == 0x00 && ard_data[6] == 0x00 && ard_data[8] = 0xFF && ard_data[9] = 0xFF):
+      		try:
+			gps_data[1] = ard_data[7]
+	except ValueError, err:
+		ard_err = True
+	return none 
+	if (ard_data[10] = == 0x00 && ard_data[11] == 0x00 && ard_data[22] = 0xFF && ard_data[23] = 0xFF):
+      		try:
+			gps_data[2] = 
+	except ValueError, err:
+		ard_err = True
+	return none
+
+	if 
 
