207752872b86c7f8a75077f559e02bc7705e7d02
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-06-05 04:22:34 -0700

    updated trapezoidal integration algorithm

--- CanSat/position.py
+++ CanSat/position.py
@@ -23,21 +23,42 @@ def get_current_trans_pos():
     return trans_pos[len(trans_pos)-1]
 def get_current_or_pos():
     return or_pos[len(or_pos)-1]
-
+def get_current_accel_data():
+    return accel_data[1]
+def get_current_mag_data():
+    return mag_data[1]
 def update_raw_data():
     global accel_data,mag_data
-    accel_data = datastore.get_accelerometer_diff()
-    mag_data   = datastore.get_magnetometer_diff() 
-    
+    #x-y-z according to the IMU will differ from x-y-z according to the module.
+    #Here x-y-z will be converted to module orientation, with:
+    #   xpos: towards front of module, viewed from top
+    #   ypos: towards right of module, viewed from top
+    #   zpos: towards top of module
+    for i in xrange(2):
+        accel_data[i][0] = -1*datastore.get_accelerometer_diff()[i][2] #x
+        accel_data[i][1] = -1*datastore.get_accelerometer_diff()[i][0] #y 
+        accel_data[i][2] = datastore.get_accelerometer_diff()[i][1] #z
+        accel_data[i][3] = datastore.get_accelerometer_diff()[i][3] #time
+
+        mag_data[i][0]   = -1*datastore.get_magnetometer_diff()[i][2]
+        mag_data[i][1]   = -1*datastore.get_magnetometer_diff()[i][0]
+        mag_data[i][2]   = datastore.get_magnetometer_diff()[i][1]
+        mag_data[i][3]   = datastore.get_magnetometer_diff()[i][3]
+    return None
+
+def trap_int(timenew, timeold, valnew, valold):
+    #Trapezoidal integration method
+    return (timenew-timeold)*(valold)+0.5*(timenew-timeold)*(valnew-valold)
+
 def calc_trans_pos():
     update_raw_data()
-    global accel_data, mag_data
+    global accel_data, mag_data, trans_pos
     #now we need to calculate the difference in accelerometer data
     #can be done by considering reimann sums
     #xDiff = (tnew-told)*(xnew+xold)*0.5
-    xDiff = 0.5*(accel_data[1][3]-accel_data[0][3])*(accel_data[1][0]-accel_data[0][0])
-    yDiff = 0.5*(accel_data[1][3]-accel_data[0][3])*(accel_data[1][1]-accel_data[0][1])
-    zDiff = 0.5*(accel_data[1][3]-accel_data[0][3])*(accel_data[1][2]-accel_data[0][2])
+    xDiff = trap_int(accel_data[1][3],accel_data[0][3],accel_data[1][0],accel_data[0][0])
+    yDiff = trap_int(accel_data[1][3],accel_data[0][3],accel_data[1][1],accel_data[0][1])
+    zDiff = trap_int(accel_data[1][3],accel_data[0][3],accel_data[1][2],accel_data[0][2])
     
     newTime = accel_data[0][3]
 
