e0c6ac9dbc89572a46587438421c130c3152fdf2
Jackrvh <Jackrvh@gmail.com>
2017-06-07 21:02:26 +0100

    Add files via upload

---
+++ BaseStation/newPro/cansat/src/cansat/DataSet.java
@@ -0,0 +1,15 @@
+package cansat;
+
+public class DataSet {
+	
+	static String header;
+	static String body;
+	static String footer;
+
+	public DataSet(String header, String body, String footer) {
+		DataSet.header = header;
+		DataSet.body = body;
+		DataSet.footer = footer;
+	}
+
+}
