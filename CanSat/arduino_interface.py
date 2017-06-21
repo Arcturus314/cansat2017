f05392026e3e7b9104aa889f1889a0ce52853cc7
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-06-21 05:49:15 -0700

    I'm an idiot

--- CanSat/arduino_interface.py
+++ CanSat/arduino_interface.py
@@ -51,7 +51,7 @@ def read_arduino(): #returns list [fix,speed,altitude,latitude,longitude]
             print "reading data..."
             for i in xrange(5): #as there are 5 data points
                 while in_char != ',': #reading the contents of each byte
-                    in_char = read_byte
+                    in_char = read_byte()
                     gps_data[i] = gps_data[i] + read_byte()
                 print "comma found"
             print "parsing data..."
