37f0291bd7794b7e07791511f248895f26613fe6
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-06-21 08:20:51 -0700

    updated launcher

--- CanSat/launcher.sh
+++ CanSat/launcher.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 # launcher.sh
 # moves to correct directory and executes python script
-# before running, confirm that the correct terminal (tty to check curren terminal) and python script are used
-# add to cron with @reboot.sh /home/chip/cansat2017/CanSat/launcher.sh >/dev/ttyGS0 2>&1
+# before running, confirm that the correct terminal (tty to check current terminal) and python script are used
+# add to cron with:
+#   1. sudo apt-get update
+#   2. sudo apt-get install cron
+#   3. sudo crontab -e
+#   4. in ~/cansat2017/CanSat run chmod -x launcher.sh
+#   4. add this line: @reboot sudo /bin/sh /home/chip/cansat2017/CanSat/launcher.sh >/dev/ttyGS0 2>&1
 
 cd /
-cd home/cansat2017/CanSat
+cd /home/chip/cansat2017/CanSat
 sudo python control.py
 cd /
 
