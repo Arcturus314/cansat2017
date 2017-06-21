792e261ceae3d4d08b3a22409e958414c7cdbeb6
Kaveh Pezeshki <kaveh@pezeshki.org>
2017-06-21 08:02:46 -0700

    updated bash launcher

--- CanSat/launcher.sh
+++ CanSat/launcher.sh
@@ -2,10 +2,10 @@
 # launcher.sh
 # moves to correct directory and executes python script
 # before running, confirm that the correct terminal (tty to check curren terminal) and python script are used
-# add to cron with @reboot.sh /home/chip/launcher.sh >/dev/ttyGS0 2>&1
+# add to cron with @reboot.sh /home/chip/cansat2017/CanSat/launcher.sh >/dev/ttyGS0 2>&1
 
 cd /
-cd home/chip
-sudo python test.py
+cd home/cansat2017/CanSat
+sudo python control.py
 cd /
 
