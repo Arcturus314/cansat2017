f24ef519d9a98e4366ce752d56c5b62be4e7fdce
CalderMacLean <caldmac08@gmail.com>
2017-06-24 17:55:58 +0100

    Update Positioning_demo.py

--- CanSat/Positioning_demo.py
+++ CanSat/Positioning_demo.py
@@ -1,1 +1,53 @@
-dzvv
+# Simple Adafruit BNO055 sensor reading example.  Will print the orientation
+# and calibration data every second.
+#
+# Copyright (c) 2015 Adafruit Industries
+# Author: Tony DiCola
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+import logging
+import sys
+import time
+import kskv
+# Create and configure the BNO sensor connection.  Make sure only ONE of the
+# below 'bno = ...' lines is uncommented:
+# Raspberry Pi configuration with serial UART and RST connected to GPIO 18:
+# BeagleBone Black configuration with default I2C connection (SCL=P9_19, SDA=P9_20),
+# and RST connected to pin P9_12:
+#bno = BNO055.BNO055(rst='P9_12')
+
+
+# Enable verbose debug logging if -v is passed as a parameter.
+if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+    logging.basicConfig(level=logging.DEBUG)
+
+# Initialize the BNO055 and stop if something went wrong.
+def get_data():
+ global heading
+ global pitch
+ global roll
+
+  heading = data.headingdata
+  pitch = data.pitchdata
+  roll = data.rolldata
+print heading
+print pitch
+print roll
+
+time.sleep(1)
