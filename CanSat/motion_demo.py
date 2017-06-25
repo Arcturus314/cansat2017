55e6afe3e3bd14846a0624dc986fed70078a4ea3
Kaveh Pezeshki <Kaveh@pezeshki.org>
2017-06-24 22:24:24 -0700

    test

--- CanSat/motion_demo.py
+++ CanSat/motion_demo.py
@@ -1,7 +1,6 @@
 import MadgwickAHRS as ahrs
 import time
 
-position_data = [0,0,0] #heading,roll,pitch
 
 sample_rate = 100
 
@@ -9,17 +8,13 @@ while True:
     global sample_rate
     global position_data
     
-    for i in xrange(10):
-        t1 = time.time()
-        data = ahrs.get_orientation(sample_rate)
-        position_data[0] = data[0]
-        position_data[1] = data[1]
-        position_data[2] = data[2]
-        t2 = time.time()
-        sample_rate = t2-t1
+    t1 = time.time()
+    data = ahrs.get_orientation(sample_rate)
+    t2 = time.time()
+    sample_rate = t2-t1
 
 
     print "Orientation:",
-    print position_data[0]/10.0,
-    print position_data[1]/10.0,
-    print position_data[2]/10.0
+    print data[0],
+    print data[1],
+    print data[2]
